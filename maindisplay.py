056a9a6322575d2e2efe1a176235650541b4de56
Thanawat sritho <64070044@kmitl.ac.th>
2023-09-29 01:43:42 +0700

    create output.py
    split function output to output .py

--- maindisplay.py
+++ maindisplay.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from pygame_widgets.slider import Slider
 from pygame_widgets.button import Button
+from output import *
 
 pygame.init()
     
@@ -23,16 +24,6 @@ top_Saturation = 0
 bottom_Saturation = 0
 top_Lightness = 0
 bottom_Lightness = 0
-
-def output(b_h, b_s, b_l, t_h, t_s, t_l):
-    cv_img = cv2.imread("pictures/room-1-with-obj.png")
-    lower = np.array([b_h, b_s, b_l])
-    upper = np.array([t_h, t_s, t_l])
-    mask = cv2.inRange(cv_img, lower, upper)
-    res = cv2.bitwise_and(cv_img, cv_img, mask= mask)
-    cv2.imwrite("pictures/outputimgs/room-1-with-obj-change.png", res)
-    global img
-    img = pygame.image.load("pictures/outputimgs/room-1-with-obj-change.png").convert_alpha()
 
 def main():
     #Slider
