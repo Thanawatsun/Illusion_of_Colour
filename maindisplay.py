7ea12ad99cafed22b27c57d91da09caca0a775cf
SSProgamer <72447877+SSProgamer@users.noreply.github.com>
2023-10-05 21:52:35 +0700

    Update img Realtime & other
    - move base img location
    - add esc key to endgame

--- maindisplay.py
+++ maindisplay.py
@@ -12,10 +12,7 @@ pygame.init()
 pygame.display.set_caption("test")
 
 #Window Size
-screen = pygame.display.set_mode((1280, 720))\
-
-#Load image
-img = pygame.image.load("pictures/room-1-with-obj.png").convert_alpha()
+screen = pygame.display.set_mode((1280, 720))
 
 #HSL
 top_hue = 0
@@ -27,12 +24,12 @@ bottom_Lightness = 0
 
 def main():
     #Slider
-    t_h_slider = Slider(screen, 725, 50, 500, 30, min=0, max=179, step=1)
-    b_h_slider = Slider(screen, 725, 150, 500, 30, min=0, max=179, step=1)
-    t_s_slider = Slider(screen, 725, 250, 500, 30, min=0, max=255, step=1)
-    b_s_slider = Slider(screen, 725, 350, 500, 30, min=0, max=255, step=1)
-    t_l_slider = Slider(screen, 725, 450, 500, 30, min=0, max=255, step=1)
-    b_l_slider = Slider(screen, 725, 550, 500, 30, min=0, max=255, step=1)
+    t_h_slider = Slider(screen, 725, 50, 500, 30, min=0, max=179, step=1, initial=179)
+    b_h_slider = Slider(screen, 725, 150, 500, 30, min=0, max=179, step=1, initial=0)
+    t_s_slider = Slider(screen, 725, 250, 500, 30, min=0, max=255, step=1, initial=255)
+    b_s_slider = Slider(screen, 725, 350, 500, 30, min=0, max=255, step=1, initial=0)
+    t_l_slider = Slider(screen, 725, 450, 500, 30, min=0, max=255, step=1, initial=255)
+    b_l_slider = Slider(screen, 725, 550, 500, 30, min=0, max=255, step=1, initial=0)
     button_positions = (1060,650)
     button_size = (200,50)
 
@@ -45,26 +42,27 @@ def main():
         for event in events:
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # get MOUSEBUTTONDOWN positions
-                if button_positions[1] <= pygame.mouse.get_pos()[1] <= button_positions[1]+button_size[1] and button_positions[0] <= pygame.mouse.get_pos()[0] <= button_positions[0]+button_size[0]:
-                    # set img when Submit
-                    output(bottom_hue, bottom_Saturation, bottom_Lightness, top_hue, top_Saturation, top_Lightness)
-                    global img
-                    img = setimg()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
         
         #Screen color | RGB
         screen.fill((128, 128, 128))
         
-        #Place image in to screen
-        screen.blit(img, (20, 20))
-
+        #Get value from slider
         top_hue = t_h_slider.getValue()
         bottom_hue = b_h_slider.getValue()
         top_Saturation = t_s_slider.getValue()
         bottom_Saturation = b_s_slider.getValue()
         top_Lightness = t_l_slider.getValue()
         bottom_Lightness = b_l_slider.getValue()
+
+        #Change output image
+        output(bottom_hue, bottom_Saturation, bottom_Lightness, top_hue, top_Saturation, top_Lightness)
+        img = setimg()
+
+        #Place image in to screen
+        screen.blit(img, (20, 20))
         
         pygame_widgets.update(events)
         pygame.display.update()
--- output.py
+++ output.py
@@ -3,7 +3,7 @@ import pygame_widgets
 import cv2
 import numpy as np
 def output(b_h, b_s, b_l, t_h, t_s, t_l):
-    cv_img = cv2.imread("pictures/room-1-with-obj.png")
+    cv_img = cv2.imread("pictures/baseimgs/room-1-with-obj.png")
     lower = np.array([b_h, b_s, b_l])
     upper = np.array([t_h, t_s, t_l])
     mask = cv2.inRange(cv_img, lower, upper)
