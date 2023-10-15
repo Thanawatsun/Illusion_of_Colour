1b699ef4f64bddb227d9ff713af7c8096b322054
SSProgamer <72447877+SSProgamer@users.noreply.github.com>
2023-10-15 21:42:11 +0700

    Make UI

--- maindisplay.py
+++ maindisplay.py
@@ -8,76 +8,24 @@ import checkresult
 pygame.init()
 
 # Window Name
-pygame.display.set_caption("gameplay")
+pygame.display.set_caption("Illusion of Colour")
 
 # Window Size
 screen = pygame.display.set_mode((1280, 720))
 
 
 def result(stage):
-    font = pygame.font.Font("freesansbold.ttf", 32)
+    btmbut = pygame.image.load("image_UI/backToStart.png").convert_alpha()
+
+    font = pygame.font.Font("freesansbold.ttf", 64)
     head = font.render("Your Score", True, (255, 255, 255))
     score = font.render(checkresult.run(stage), True, (255, 255, 255))
-    while True:
-        screen.fill((128, 128, 128))
-
-        screen.blit(head, (150, 150))
-        screen.blit(score, (200, 200))
-
-        mouse_location = pygame.mouse.get_pos()
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    menu()
-
-        checkresult.run(stage)
-
-        pygame_widgets.update(events)
-        pygame.display.update()
-
-
-def main(stage):
-    # HSL
-    top_hue = 0
-    bottom_hue = 0
-    top_Saturation = 0
-    bottom_Saturation = 0
-    top_Lightness = 0
-    bottom_Lightness = 0
-
-    # Slider
-    t_h_slider = Slider(screen, 725, 50, 500, 30, min=0, max=255, step=1, initial=255)
-    b_h_slider = Slider(screen, 725, 150, 500, 30, min=0, max=255, step=1, initial=0)
-    t_s_slider = Slider(screen, 725, 250, 500, 30, min=0, max=255, step=1, initial=255)
-    b_s_slider = Slider(screen, 725, 350, 500, 30, min=0, max=255, step=1, initial=0)
-    t_l_slider = Slider(screen, 725, 450, 500, 30, min=0, max=255, step=1, initial=255)
-    b_l_slider = Slider(screen, 725, 550, 500, 30, min=0, max=255, step=1, initial=0)
-    button_positions = (1060, 650)
-    button_size = (200, 50)
-
-    # Make button | (window, location-X, location-Y, Width, Height)
-    submit = Button(
-        screen,
-        button_positions[0],
-        button_positions[1],
-        button_size[0],
-        button_size[1],
-        text="Submit",
-        inactiveColour=(200, 50, 0),
-    )
 
     while True:
-        # Screen color | RGB
         screen.fill((128, 128, 128))
-
-        img = setimg(stage)
-
-        # Place image in to screen
-        screen.blit(img, (20, 20))
+        screen.blit(head, (475, 165))
+        screen.blit(score, (491, 299))
+        screen.blit(btmbut, (393, 433))
 
         mouse_location = pygame.mouse.get_pos()
         events = pygame.event.get()
@@ -89,10 +37,72 @@ def main(stage):
                 mouse_press = pygame.mouse.get_pressed()
                 if (
                     mouse_press[0]
-                    and mouse_location[0] >= 1060
-                    and mouse_location[0] <= 1260
-                    and mouse_location[1] >= 650
-                    and mouse_location[1] <= 700
+                    and mouse_location[0] >= 393
+                    and mouse_location[0] <= 886
+                    and mouse_location[1] >= 433
+                    and mouse_location[1] <= 558
+                ):
+                    menu()
+
+        checkresult.run(stage)
+
+        pygame_widgets.update(events)
+        pygame.display.update()
+
+
+def main(stage):
+    # Main background
+    stagebg = "image_UI/backgroud-0%d.png" % stage
+    bgimg = pygame.image.load(stagebg).convert_alpha()
+    bgimgframe = pygame.image.load("image_UI/imageFrame.png").convert_alpha()
+    bgwantframe = pygame.image.load("image_UI/wantFrame.png").convert_alpha()
+    bgsliderframe = pygame.image.load("image_UI/sliderFrame.png").convert_alpha()
+    submitbut = pygame.image.load("image_UI/submit.png").convert_alpha()
+
+    # HSL
+    top_hue = 0
+    bottom_hue = 0
+    top_Saturation = 0
+    bottom_Saturation = 0
+    top_Lightness = 0
+    bottom_Lightness = 0
+
+    # Slider
+    t_h_slider = Slider(screen, 825, 75, 400, 25, min=0, max=255, step=1, initial=255)
+    b_h_slider = Slider(screen, 825, 175, 400, 25, min=0, max=255, step=1, initial=0)
+    t_s_slider = Slider(screen, 825, 275, 400, 25, min=0, max=255, step=1, initial=255)
+    b_s_slider = Slider(screen, 825, 375, 400, 25, min=0, max=255, step=1, initial=0)
+    t_l_slider = Slider(screen, 825, 475, 400, 25, min=0, max=255, step=1, initial=255)
+    b_l_slider = Slider(screen, 825, 575, 400, 25, min=0, max=255, step=1, initial=0)    
+
+    while True:
+        # Screen color | RGB
+        screen.fill((128, 128, 128))
+        screen.blit(bgimg, (0, 0))
+        screen.blit(bgimgframe, (23, 25))
+        screen.blit(bgwantframe, (45, 492))
+        screen.blit(bgsliderframe, (792, 26))
+        screen.blit(submitbut, (820, 620))
+
+        img = setimg(stage)
+
+        # Place image in to screen
+        screen.blit(img, (73, 50))
+
+        mouse_location = pygame.mouse.get_pos()
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_press = pygame.mouse.get_pressed()
+                if (
+                    mouse_press[0]
+                    and mouse_location[0] >= 820
+                    and mouse_location[0] <= 1230
+                    and mouse_location[1] >= 620
+                    and mouse_location[1] <= 686
                 ):
                     t_h_slider.hide()
                     b_h_slider.hide()
@@ -100,7 +110,6 @@ def main(stage):
                     b_s_slider.hide()
                     t_l_slider.hide()
                     b_l_slider.hide()
-                    submit.hide()
                     result(stage)
 
         # Get value from slider
@@ -126,12 +135,17 @@ def main(stage):
         pygame.display.update()
 
 def chosestage():
+    backbut = pygame.image.load("image_UI/back.png").convert_alpha()
     stage1 = pygame.image.load("image_UI/stage01.png").convert_alpha()
+    stage2 = pygame.image.load("image_UI/stage02.png").convert_alpha()
+    stage3 = pygame.image.load("image_UI/stage03.png").convert_alpha()
 
     while True:
         screen.fill((128, 128, 128))
-
-        screen.blit(stage1, (0, 0))
+        screen.blit(backbut, (19, 15))
+        screen.blit(stage1, (231, 235))
+        screen.blit(stage2, (515, 235))
+        screen.blit(stage3, (799, 235))
 
         mouse_location = pygame.mouse.get_pos()
         events = pygame.event.get()
@@ -143,34 +157,46 @@ def chosestage():
                 mouse_press = pygame.mouse.get_pressed()
                 if (
                     mouse_press[0]
-                    and mouse_location[0] >= 0
-                    and mouse_location[0] <= 200
-                    and mouse_location[1] >= 0
-                    and mouse_location[1] <= 200
+                    and mouse_location[0] >= 19
+                    and mouse_location[0] <= 94
+                    and mouse_location[1] >= 15
+                    and mouse_location[1] <= 93
+                ):
+                    menu()
+                elif (
+                    mouse_press[0]
+                    and mouse_location[0] >= 231
+                    and mouse_location[0] <= 481
+                    and mouse_location[1] >= 235
+                    and mouse_location[1] <= 485
                 ):
                     main(1)
+                elif (
+                    mouse_press[0]
+                    and mouse_location[0] >= 515
+                    and mouse_location[0] <= 765
+                    and mouse_location[1] >= 235
+                    and mouse_location[1] <= 485
+                ):
+                    main(2)
+                elif (
+                    mouse_press[0]
+                    and mouse_location[0] >= 799
+                    and mouse_location[0] <= 1049
+                    and mouse_location[1] >= 235
+                    and mouse_location[1] <= 485
+                ):
+                    main(3)
 
         pygame_widgets.update(events)
         pygame.display.update()
 
-
-def menu():
-    button_positions = (560, 650)
-    button_size = (200, 50)
-
-    # Make button | (window, location-X, location-Y, Width, Height)
-    button = Button(
-        screen,
-        button_positions[0],
-        button_positions[1],
-        button_size[0],
-        button_size[1],
-        text="Start",
-        inactiveColour=(200, 50, 0),
-    )
-    # Screen color | RGB
-    screen.fill((128, 128, 128))
+def help():
+    returnbut = pygame.image.load("image_UI/return.png").convert_alpha()
     while True:
+        screen.fill((128, 128, 128))
+        screen.blit(returnbut, (490, 568))
+
         mouse_location = pygame.mouse.get_pos()
         events = pygame.event.get()
         for event in events:
@@ -181,14 +207,60 @@ def menu():
                 mouse_press = pygame.mouse.get_pressed()
                 if (
                     mouse_press[0]
-                    and mouse_location[0] >= 560
-                    and mouse_location[0] <= 760
-                    and mouse_location[1] >= 650
-                    and mouse_location[1] <= 700
+                    and mouse_location[0] >= 490
+                    and mouse_location[0] <= 790
+                    and mouse_location[1] >= 568
+                    and mouse_location[1] <= 692
                 ):
-                    # The button is clicked, perform your action
-                    button.hide()  # Hide the button
+                    menu()
+
+        pygame_widgets.update(events)
+        pygame.display.update()
+
+def menu():
+    playbut = pygame.image.load("image_UI/start.png").convert_alpha()
+    helpbut = pygame.image.load("image_UI/help.png").convert_alpha()
+    exitbut = pygame.image.load("image_UI/exit.png").convert_alpha()
+    
+    while True:
+        screen.fill((128, 128, 128))
+        screen.blit(playbut, (490, 153))
+        screen.blit(helpbut, (490, 300))
+        screen.blit(exitbut, (490, 447))
+
+        mouse_location = pygame.mouse.get_pos()
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_press = pygame.mouse.get_pressed()
+                if (
+                    mouse_press[0]
+                    and mouse_location[0] >= 490
+                    and mouse_location[0] <= 790
+                    and mouse_location[1] >= 153
+                    and mouse_location[1] <= 277
+                ):
                     chosestage()
+                elif (
+                    mouse_press[0]
+                    and mouse_location[0] >= 490
+                    and mouse_location[0] <= 790
+                    and mouse_location[1] >= 300
+                    and mouse_location[1] <= 424
+                ):
+                    help()
+                elif (
+                    mouse_press[0]
+                    and mouse_location[0] >= 490
+                    and mouse_location[0] <= 790
+                    and mouse_location[1] >= 447
+                    and mouse_location[1] <= 571
+                ):
+                    pygame.quit()
+                    sys.exit()
 
         pygame_widgets.update(events)
         pygame.display.update()
