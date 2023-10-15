aa59ebee1b1f48af979624ea43571b8548e6aa62
SSProgamer <72447877+SSProgamer@users.noreply.github.com>
2023-10-15 16:49:06 +0700

    Prepare for multiple stage

--- output.py
+++ output.py
@@ -3,16 +3,19 @@ import pygame_widgets
 import cv2
 import numpy as np
 
-def output(b_h, b_s, b_l, t_h, t_s, t_l):
-    test = "pictures/baseimgs/room-1-with-obj.png"
-    cv_img = cv2.imread(test)
+def output(b_h, b_s, b_l, t_h, t_s, t_l, stage):
+    baseimg = "pictures/baseimgs/room-%d-with-obj.png" % stage
+    changeimg = "pictures/outputimgs/room-%d-with-obj-change.png" % stage
+
+    cv_img = cv2.imread(baseimg)
     lower = np.array([b_h, b_s, b_l])
     upper = np.array([t_h, t_s, t_l])
     mask = cv2.inRange(cv_img, lower, upper)
     res = cv2.bitwise_and(cv_img, cv_img, mask=mask)
-    cv2.imwrite("pictures/outputimgs/room-1-with-obj-change.png", res)
+    cv2.imwrite(changeimg, res)
 
 
-def setimg():
-    img = pygame.image.load("pictures/outputimgs/room-1-with-obj-change.png").convert_alpha()
+def setimg(stage):
+    changeimg = "pictures/outputimgs/room-%d-with-obj-change.png" % stage
+    img = pygame.image.load(changeimg).convert_alpha()
     return img
